9824c8a1bbce8eca5e2f816036d5dec9cd6c53b8
Mohamed Wajih <wajih.tagourty@gmail.com>
2021-03-02 21:28:47 +0100

    Fixing search type issue break point

--- src/pages/partials/SearchResults.tsx
+++ src/pages/partials/SearchResults.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Issue } from "../../components/cards/Issue";
 import { RootState } from "../../features/rootReducer";
+import { SearchTypes } from "../../features/search/types";
 
 interface SearchResultsProps { }
 
 export const SearchResults: React.FC<SearchResultsProps> = ({ }) => {
+  const resultsType = useSelector((state: RootState) => state.search.type);
   const results = useSelector((state: RootState) => state.search.result);
+
+
+
+    /*
+    results is of type Array<GithubItemsType />, 
+    we want to check which type are we talking about
+    GithubIssue, GithubRepo or GithubUser and convert it
+    */
+  
+
+
   return (
     <div className="results">
-      <p>
-        fetched <span>{results?.length}  </span>
-      </p>
+      <p>fetched <span>{results?.length} {resultsType}  </span></p>
     </div>
   );
 };
