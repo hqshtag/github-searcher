8156a8af8525c0f300314e3dba44479e55c0af1c
Mohamed Wajih <wajih.tagourty@gmail.com>
2021-03-02 22:35:27 +0100

    printed the results to screen with minimum success

--- src/pages/partials/SearchResults.tsx
+++ src/pages/partials/SearchResults.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { isGithubUser, isGithubIssue, isGithubRepository } from "../../api/GithubTypes";
 import { Issue } from "../../components/cards/Issue";
+import { Repository } from "../../components/cards/Repository";
+import { User } from "../../components/cards/User";
 import { RootState } from "../../features/rootReducer";
 import { SearchTypes } from "../../features/search/types";
 
@@ -11,18 +14,28 @@ export const SearchResults: React.FC<SearchResultsProps> = ({ }) => {
   const results = useSelector((state: RootState) => state.search.result);
 
 
-
-    /*
-    results is of type Array<GithubItemsType />, 
-    we want to check which type are we talking about
-    GithubIssue, GithubRepo or GithubUser and convert it
-    */
-  
+  const Result = () => {
+    return (
+      <div className="results-container">
+        {results?.map((e, i) => {
+          if (isGithubUser(e)) {
+            return <User user={e} key={i} />
+          } else if (isGithubRepository(e)) {
+            return <Repository repository={e} key={i} />
+          } else if (isGithubIssue(e)) {
+            return <Issue issue={e} key={i} />
+          }
+        })}
+      </div>
+    )
+    
+  }
 
 
   return (
     <div className="results">
       <p>fetched <span>{results?.length} {resultsType}  </span></p>
+      <Result />
     </div>
   );
 };
