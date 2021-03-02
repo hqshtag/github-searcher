176513e1857d62143db59e15414d78c82147d5be
Mohamed Wajih <wajih.tagourty@gmail.com>
2021-03-02 22:34:53 +0100

    Added typeguard for each github item types

--- src/api/GithubServices.ts
+++ src/api/GithubServices.ts
@@ -3,8 +3,8 @@ import { SearchTDO } from "../features/search/types";
 import { GithubErrorResponse, GithubResponseObject } from "./GithubTypes";
 
 
-export const SearchGithubAPI = async (data: SearchTDO): Promise<GithubResponseObject | GithubErrorResponse> => {
+export const SearchGithubAPI = async (data: SearchTDO, page = 1, per_page = 42): Promise<GithubResponseObject | GithubErrorResponse> => {
   console.log(data);
-  return await (await axios.get(`https://api.github.com/search/${data.type}?q=${data.text}`)).data;
+  return await (await axios.get(`https://api.github.com/search/${data.type}?q=${data.text}&page=${page}&per_page=${per_page}`)).data;
 }
 
--- src/api/GithubTypes.ts
+++ src/api/GithubTypes.ts
@@ -1,160 +1,188 @@
-
 export interface GithubResponseObject {
-  count: number
-  incomplete_results: Boolean
-  items: [GithubItemsType]
+  items_count: number;
+  incomplete_results: Boolean;
+  items: [GithubItemsType];
 }
 
 export interface GithubErrorResponse {
-  message: string,
-  documentation_url: string
+  message: string;
+  documentation_url: string;
 }
 
 //the three types will have comm
 export type GithubItemsType = GithubIssue | GithubRepository | GithubUser;
 
 export interface GithubUser {
-  login: string
-  id: number
-  node_id: string
-  avatar_url: string
-  gravatar_id: any
-  url: string
-  html_url: string
-  followers_url: string
-  gists_url: string
-  starred_url: string
-  subscriptions_url: string
-  organizations_url: string
-  repos_url: string
-  events_url: string
-  received_events_url: string
-  type: string
-  site_admin: Boolean
-  score: number
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: any;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: Boolean;
+  score: number;
 }
 
 export interface GithubRepository {
-  id: number
-  node_id: string,
-  name: string,
-  full_name: string,
-  private: Boolean,
-  owner: GithubUser,
-  html_url: string,
-  description: string,
-  fork: Boolean,
-  url: string,
-  forks_url: string,
-  keys_url: string,
-  collaborators_url: string,
-  teams_url: string,
-  hooks_url: string,
-  issue_events_url: string,
-  events_url: string,
-  assignees_url: string,
-  branches_url: string,
-  tags_url: string,
-  blobs_url: string,
-  git_tags_url: string,
-  git_refs_url: string,
-  trees_url: string,
-  statuses_url: string,
-  languages_url: string,
-  stargazers_url: string,
-  contributors_url: string,
-  subscribers_url: string,
-  subscription_url: string,
-  commits_url: string,
-  git_commits_url: string,
-  comments_url: string,
-  issue_comment_url: string,
-  contents_url: string,
-  compare_url: string,
-  merges_url: string,
-  archive_url: string,
-  downloads_url: string,
-  issues_url: string,
-  pulls_url: string,
-  milestones_url: string,
-  notifications_url: string,
-  labels_url: string,
-  releases_url: string,
-  deployments_url: string,
-  created_at: string,
-  updated_at: string,
-  pushed_at: string,
-  git_url: string,
-  ssh_url: string,
-  clone_url: string,
-  svn_url: string,
-  homepage: string,
-  size: number,
-  stargazers_count: number,
-  watchers_count: number,
-  language: string,
-  has_issues: Boolean,
-  has_projects: Boolean,
-  has_downloads: Boolean,
-  has_wiki: Boolean,
-  has_pages: Boolean,
-  forks_count: number,
-  mirror_url: any,
-  archived: Boolean,
-  disabled: Boolean,
-  open_issues_count: number,
-  license: GithubLicence,
-  forks: number,
-  open_issues: number,
-  watchers: number,
-  default_branch: string,
-  score: number
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  private: Boolean;
+  owner: GithubUser;
+  html_url: string;
+  description: string;
+  fork: Boolean;
+  url: string;
+  forks_url: string;
+  keys_url: string;
+  collaborators_url: string;
+  teams_url: string;
+  hooks_url: string;
+  issue_events_url: string;
+  events_url: string;
+  assignees_url: string;
+  branches_url: string;
+  tags_url: string;
+  blobs_url: string;
+  git_tags_url: string;
+  git_refs_url: string;
+  trees_url: string;
+  statuses_url: string;
+  languages_url: string;
+  stargazers_url: string;
+  contributors_url: string;
+  subscribers_url: string;
+  subscription_url: string;
+  commits_url: string;
+  git_commits_url: string;
+  comments_url: string;
+  issue_comment_url: string;
+  contents_url: string;
+  compare_url: string;
+  merges_url: string;
+  archive_url: string;
+  downloads_url: string;
+  issues_url: string;
+  pulls_url: string;
+  milestones_url: string;
+  notifications_url: string;
+  labels_url: string;
+  releases_url: string;
+  deployments_url: string;
+  created_at: string;
+  updated_at: string;
+  pushed_at: string;
+  git_url: string;
+  ssh_url: string;
+  clone_url: string;
+  svn_url: string;
+  homepage: string;
+  size: number;
+  stargazers_count: number;
+  watchers_count: number;
+  language: string;
+  has_issues: Boolean;
+  has_projects: Boolean;
+  has_downloads: Boolean;
+  has_wiki: Boolean;
+  has_pages: Boolean;
+  forks_count: number;
+  mirror_url: any;
+  archived: Boolean;
+  disabled: Boolean;
+  open_issues_count: number;
+  license: GithubLicence;
+  forks: number;
+  open_issues: number;
+  watchers: number;
+  default_branch: string;
+  score: number;
 }
 
 interface GithubLicence {
-  key: string,
-  name: string,
-  spdx_id: string,
-  url: string,
-  node_id: string
+  key: string;
+  name: string;
+  spdx_id: string;
+  url: string;
+  node_id: string;
 }
 
 export interface GithubIssue {
-  url: string,
-  repository_url: string,
-  labels_url: string,
-  comments_url: string,
-  events_url: string,
-  html_url: string,
-  id: number,
-  node_id: string,
-  number: number,
-  title: string,
-  user: GithubUser
-  labels: [],
-  state: string,
-  locked: Boolean,
-  assignee: any,
-  assignees: [],
-  milestone: any,
-  comments: number,
-  created_at: string,
-  updated_at: string,
-  closed_at: any,
-  author_association: string,
-  active_lock_reason: any,
-  body: string,
-  performed_via_github_app: any,
-  score: number
+  url: string;
+  repository_url: string;
+  labels_url: string;
+  comments_url: string;
+  events_url: string;
+  html_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  title: string;
+  user: GithubUser;
+  labels: [];
+  state: string;
+  locked: Boolean;
+  assignee: any;
+  assignees: [];
+  milestone: any;
+  comments: number;
+  created_at: string;
+  updated_at: string;
+  closed_at: any;
+  author_association: string;
+  active_lock_reason: any;
+  body: string;
+  performed_via_github_app: any;
+  score: number;
 }
-
-
-
 
 //This is a type guard/check for github response
-export const ReachedOurRateLimit = (toBeDetermined: GithubErrorResponse | GithubResponseObject): toBeDetermined is GithubErrorResponse => {
+export const ReachedOurRateLimit = (
+  toBeDetermined: GithubErrorResponse | GithubResponseObject
+): toBeDetermined is GithubErrorResponse => {
   if ((toBeDetermined as GithubErrorResponse).message) {
-    return true
+    return true;
   }
-  return false
+  return false;
+};
+
+//typeguards  for each GithubItem
+export const isGithubUser = (
+  toBeDetermined: any
+): toBeDetermined is GithubUser => {
+  if ((toBeDetermined as GithubUser).login) {
+    return true;
+  }
+  return false;
+};
+
+export const isGithubRepository = (
+  toBeDetermined: any
+): toBeDetermined is GithubRepository => {
+  if ((toBeDetermined as GithubRepository).name) {
+    return true;
+  }
+  return false;
 }
+
+
+export const isGithubIssue = (
+  toBeDetermined: any
+): toBeDetermined is GithubIssue => {
+  if ((toBeDetermined as GithubIssue).title) {
+    return true;
+  }
+  return false;
+}
+
