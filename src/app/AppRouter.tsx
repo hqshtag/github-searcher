6a44b1d3d2b6805e730389c5f0277e175b8b944f
hqshtag <wajih.tagourty@gmail.com>
2022-04-20 00:24:53 +0000

    fixed ts errors

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,27 +1,21 @@
-import React, { useEffect } from 'react'
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import { SearchPage } from '../pages/SearchPage';
+import React, { useEffect } from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import { SearchPage } from "../pages/SearchPage";
 
+export const AppRouter: React.FC = () => {
+  useEffect(() => {
+    const knownPaths = ["/search"];
 
-export const AppRouter: React.FC = ({ }) => {
+    if (!knownPaths.includes(window.location.pathname)) {
+      window.location.replace("/search");
+    }
+  }, []);
 
-    const knownPaths = [
-        "/search",
-    ];
-
-    useEffect(() => {
-        if (
-            !knownPaths.includes(window.location.pathname)
-        ) {
-            window.location.replace("/search");
-        }
-    }, [window.location.pathname])
-
-    return (
-        <Router>
-            <Route exact path="/search">
-                <SearchPage />
-            </Route>
-        </Router>
-    );
-}
+  return (
+    <Router>
+      <Route exact path="/search">
+        <SearchPage />
+      </Route>
+    </Router>
+  );
+};
